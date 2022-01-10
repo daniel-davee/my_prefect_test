25c0195634ce69753c89537834ff071f4a0cd3d0
daniel-davee <daniel.v.davee@gmail.com>
2022-01-09 20:00:24 -0700

    hopefully the git storage works

--- flows/flow.py
+++ flows/flow.py
@@ -1,13 +1,13 @@
-from os import name
 import prefect
 from prefect import task, Flow
 from prefect.tasks.shell import ShellTask
+from prefect.storage.git import Git
 
 @task
 def hello_task():
     logger = prefect.context.get("logger")
-    logger.info("Hello world!")
-    return 'touch howdy'
+    logger.info("howdy")
+    return 'touch howdy-from-git-hub'
 
 run_in_shell = ShellTask(name = 'shell_test')
 
@@ -15,4 +15,6 @@ with Flow("hello-flow") as flow:
     cmd = hello_task()
     run_in_shell(command = cmd)
 
-flow.run()
+flow.storage = Git(flow_path = '/flows/flow.py', 
+                   repo = 'daniel-davee/my_prefect_test',
+                   repo_host = 'github.com')
